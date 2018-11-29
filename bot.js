1195c3e6f7918912f38ccd6ea0895a413e826842
AbdulazizAlqahtani <45456609+AbdulazizAlqahtani@users.noreply.github.com>
2018-11-29 17:10:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,88 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
+const client2 = new Discord.Client();
+
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+   console.log(`----------------`);
+   console.log(`Credit Farmm - Script By : Kahrbaa `);
+   console.log(`----------------`);
+   console.log(`Loadinng`);
+   console.log(`Loadinng.`);
+   console.log(`Loadinng..`);
+   console.log(`Loadinng...`);
+   console.log(`This Bots Online ' `);
+   console.log(`----------------`);
 });
 
 
-client.on('ready', async() => {
-var server = "515429310991958016"; // ايدي السررفر
-var channel = "516904167248494602";//ايدي الروم
-    setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('#u,Hazard , Relo ,#u,Hazard , Relo ,#u,Hazard , Relo ,#u,Hazard , Relo ,#u,Hazard , Relo ,#u,Hazard , Relo ,#u,Hazard , Relo ,#u,Hazard , Relo ,#u,Hazard , Relo ,#u,Hazard , Relo ,#u,Hazard , Relo ,#u,Hazard , Relo ,#u,Hazard , Relo ,#u,Hazard , Relo ,#u,Hazard , Relo ,#u,Hazard , Relo ,#u,Hazard , Relo ,#u,Hazard , Relo ,#u,Hazard , Relo ,#u,Hazard , Relo ,#u,Hazard , Relo , ')
-    },305);
-})
+client.on('message', message => {
+    if(message.content === '!ديلي'){
+        message.channel.send('#daily')
+    }
+});
 
-client.login(process.env."NTE3MzQwMjgxNTU3ODc2NzU3.DuF0ig.bMS_o4QJTNVEyctgW08yfHTkoEs");
+client.on('message', message => {
+    if(message.content === '!كريدت'){
+        message.channel.send('#credits')
+    }
+});
+
+client.on('message', message => {
+    if(message.content === '!كريدت'){
+        message.channel.send('#credits')
+    }
+});
+
+client2.on('message', message => {
+    if(message.content === '!ديلي'){
+        message.channel.send('#daily')
+    }
+});
+
+client2.on('message', message => {
+    if(message.content === '!كريدت'){
+        message.channel.send('#credits')
+    }
+});
+
+client2.on('message', message => {
+    if(message.content === '!كريدت'){
+        message.channel.send('#credits')
+    }
+});
+
+
+
+client.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
+if (message.content === '!سبآم') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client2.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
+if (message.content === '!سبآم') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi ,**[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+
+
+
+client.login(process.env.TOKEN);// لا تغير فيها شيء
+client2.login(process.env.TOKEN2);// لا تغير فيها شيء
